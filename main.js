f3e59ee5fec129aa1697f4a37557388b9d36dadc
RustyNox <noelmdixon@gmail.com>
2022-06-01 22:37:15 +1000

    Refactor env file ref and connect database

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const { MongoClient } = require('mongodb');
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
+// if (process.env.NODE_ENV !== 'production') {
+require('dotenv').config();
+// }
 
 async function main() {
-    const uri = "mongodb+srv://admin:<password>@cluster0.jgckh.mongodb.net/?retryWrites=true&w=majority";
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgckh.mongodb.net/?retryWrites=true&w=majority`;
+
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 }
+
+main().catch(console.error);
