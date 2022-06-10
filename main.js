c61e8e791faf42067bcd90d6ea109e4df25ed611
RustyNox <noelmdixon@gmail.com>
2022-06-10 12:28:55 +1000

    Add create single record query and call

--- main.js
+++ main.js
@@ -16,6 +16,16 @@ async function main() {
 
 
 
+        // Create single record
+        await createListing(client,
+            {
+                name: "Lovely Loft",
+                summary: "A charming loft in central Sydney",
+                bedrooms: 1,
+                bathrooms: 1
+            }
+        );
+
         //List databases
         await listDatabases(client);
 
@@ -29,6 +39,13 @@ async function main() {
 main().catch(console.error);
 
 
+
+// Create single record
+async function createListing(client, newListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+
+    console.log(`New listing created with the following id: ${result.insertedId}`);
+}
 
 // List databases
 async function listDatabases(client) {
